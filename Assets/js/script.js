43e22b1e2b522201c3de5056dfa404271ccbf715
Luis Debourg <Luisdebourgm@gmail.com>
2022-03-27 16:24:20 +1100

    time added to the jumbotron section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,12 @@
+//ready method will run this funtion as soon as the website is open 
 $(document).ready(function () {
     var date = moment().format('dddd MMMM Do');
     $('#currentDay').text(date);
 })
+
+//when it opens it should change the colour of the text areas when is past it should be grey
+//when its present should be red and future in green
+
+//if time is past it should be grey
+//if the time is the curent time it should be red 
+// the future time should be green
