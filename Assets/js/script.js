0fec20291056cba0ae862fb9a1e645a6c15921f5
Luis Debourg <Luisdebourgm@gmail.com>
2022-03-30 21:09:54 +1100

    working code modified code, update read me file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,29 +3,51 @@ $(document).ready(function () {
     var date = moment().format('dddd MMMM Do hh:mm:ss');
     $('#currentDay').text(date);
 
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        localStorage.setItem(text);
-    })
-});
-
-// function currentTime(){
-//     var current = moment().hour();
-    
-// }
-// console.log(currentTime);
-function currentTime() {
+    function currentTime() {
         var timeNow = moment().hour();
+        
        
+   // same a loop this funtion will check each div time and compared it with the current time 
+   //it will change the text area container according with the time 
+    $('.time-block').each(function(){
+        var hours = parseInt($(this).attr('id'))
+//      if the curent time is equal to the time in the div hour it will create a class 'present'
+//      wich will make the text area red
+        if (hours === timeNow) {
+            $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
+   //      if the curent time is less to the time in the div hour it will create a class 'past'
+//      wich will make the text area  grey      
+          } else if (timeNow > hours) {
+            $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
+        // if not it will create a class wich will make the text area green
+          } else {
+            $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
+        
+          }
+        })
     };
-currentTime();
 
- 
- 
-//when it opens it should change the colour of the text areas when is past it should be grey
-//when its present should be red and future in green
+    // when the button is click it would save the hour and the value typed in the textarea 
+    // and save it in the localstorage
+    $(".saveBtn").on("click", function (event) {
+       
+        var time = $(this).siblings(".hour").text();
+        var text = $(this).siblings(".description").val();
+        
+        localStorage.setItem(time, text);
+    })
 
-//if time is past it should be grey
-//if the time is the curent time it should be red 
-// the future time should be green
+    // this will leave the value of the imput typed in textarea printed
+    $("#9 .description").val(localStorage.getItem("9AM"));
+    $("#10 .description").val(localStorage.getItem("10AM"));
+    $("#11 .description").val(localStorage.getItem("11AM"));
+    $("#12 .description").val(localStorage.getItem("12PM"));
+    $("#13 .description").val(localStorage.getItem("1PM"));
+    $("#14 .description").val(localStorage.getItem("2PM"));
+    $("#15 .description").val(localStorage.getItem("3PM"));
+    $("#16 .description").val(localStorage.getItem("4PM"));
+    $("#17 .description").val(localStorage.getItem("5PM"));
+
+    currentTime();
+});
 
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Third-Party-APIs-Work-Day-Scheduler
 create a work schedule using third party api 
+
+a work day scheduler created using third party api,
+using boostrap, jquery and js.moment.
+
+using the local storage to save the input and keep it in the screen.
+it change the colour of the box area to indicate if the task is in the present,past or future.
+
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/img/05-third-party-apis-homework-demo.gif)
+
+URL to website
+[Link to Website](https://fadesoner.github.io/Third-Party-APIs-Work-Day-Scheduler/)
+
+URL to github repository
+[Link to Github](https://github.com/FadesOner)
